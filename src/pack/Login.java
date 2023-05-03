dc02da4810a34f65e51cefe05b8432e9e8154172
GhoshAnujit <118505475+GhoshAnujit@users.noreply.github.com>
2023-05-03 15:35:14 +0530

    LOL

--- src/pack/Login.java
+++ src/pack/Login.java
@@ -95,8 +95,8 @@ public class Login extends JFrame {
 		panel_2.setLayout(null);
 		
 		//Label for Menu Bar text
-		String User = "Keegan";
-		JLabel lblNewLabel_3 = new JLabel("Welcome "+User);
+		//String User = "Keegan";
+		JLabel lblNewLabel_3 = new JLabel("Welcome to Play Pro !");
 		lblNewLabel_3.setForeground(new Color(0, 0, 0));
 		lblNewLabel_3.setFont(new Font("Monotype corsiva", Font.PLAIN, 36));
 		lblNewLabel_3.setBounds(579, 0, 735, 75);
@@ -125,72 +125,75 @@ public class Login extends JFrame {
 		
 		//Button for list of possible bookings
 		
-		ImageIcon icon1 = new ImageIcon(Newframe.class.getResource("sking.jpg"));
+		ImageIcon icon1 = new ImageIcon(Newframe.class.getResource("bbct.jpg"));
 		Image image1 = icon1.getImage();
 		Image new1 = image1.getScaledInstance(273,132,java.awt.Image.SCALE_SMOOTH);
 		ima = new ImageIcon(new1);
-		panel_3.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5)); 
-		
-		Image image3 = icon1.getImage();
-		new2 = image3.getScaledInstance(273,132,java.awt.Image.SCALE_SMOOTH);
-		ima = new ImageIcon(new2);
-		JButton btnNewButton_2 = new JButton(ima);
-		
-		
-		//Don't do this, create a new swing application window, for pop-up, and then call it in your button , ovveriding the class
-		// in that way, you can deactivate the exit on close(just comment it out), so when you close your frame, all the frames 
-		// don't close along with it. Remember that you can only change the variables that you get from the user through imput, but 
-		// a single frame will carry with it, it's characteristics, like it ability to exit on close, etc.
-		
-		
-		
-		btnNewButton_2.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				//VENUEPOP vp = new VENUEPOP();
-				//vp.NewScreen();
+		panel_3.setLayout(null);
+		JButton btnNewButton_1 = new JButton(ima);
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				LOGINPOP loginn = new LOGINPOP();
+				loginn.lg();
 			}
 		});
+		btnNewButton_1.setBounds(32, 32, 282, 141);
+		btnNewButton_1.setToolTipText("A list of all the venues");
+		panel_3.add(btnNewButton_1);
+		
+		ImageIcon icon3 = new ImageIcon(Newframe.class.getResource("ltct.jpg"));
+		Image image3 = icon3.getImage();
+		Image new3 = image3.getScaledInstance(273,132,java.awt.Image.SCALE_SMOOTH);
+		ima = new ImageIcon(new3);
+		panel_3.setLayout(null);
+		JButton btnNewButton_2 = new JButton(ima);
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				//Setting up a new pop-up frame for venue button
-			//	VENUEPOP vp = new VENUEPOP();
-				//vp.NewScreen();
-				
-		}
-	});
+				LOGINPOP loginn1 = new LOGINPOP();
+				loginn1.lg();
+			}
+		});
+		btnNewButton_2.setBounds(343, 189, 282, 141);
 		btnNewButton_2.setToolTipText("A list of all the venues");
-		btnNewButton_2.setBounds(66, 10, 273, 132);
 		panel_3.add(btnNewButton_2);
 		
-		JButton btnNewButton_2_1 = new JButton(ima);
-		btnNewButton_2.addActionListener(new ActionListener() {
+		ImageIcon icon4 = new ImageIcon(Newframe.class.getResource("ttct.jpg"));
+		Image image4 = icon4.getImage();
+		Image new4 = image4.getScaledInstance(273,132,java.awt.Image.SCALE_SMOOTH);
+		ima = new ImageIcon(new4);
+		panel_3.setLayout(null);
+		JButton btnNewButton_3 = new JButton(ima);
+		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				//Setting up a new pop-up frame for venue button
-				//VENUEPOP vp = new VENUEPOP();
-				//vp.NewScreen();
-				
+				LOGINPOP loginn2 = new LOGINPOP();
+				loginn2.lg();
 			}
 		});
-		btnNewButton_2.setToolTipText("A list of all the venues");
-		btnNewButton_2.setBounds(66, 10, 273, 132);
-		panel_3.add(btnNewButton_2_1);
+		btnNewButton_3.setBounds(637, 32, 282, 141);
+		btnNewButton_3.setToolTipText("A list of all the venues");
+		panel_3.add(btnNewButton_3);
 		
-		JButton btnNewButton_2_2 = new JButton(ima);
-		btnNewButton_2.addActionListener(new ActionListener() {
+		ImageIcon icon5 = new ImageIcon(Newframe.class.getResource("ftct.jpg"));
+		Image image5 = icon5.getImage();
+		Image new5 = image5.getScaledInstance(273,132,java.awt.Image.SCALE_SMOOTH);
+		ima = new ImageIcon(new5);
+		panel_3.setLayout(null);
+		JButton btnNewButton_4 = new JButton(ima);
+		btnNewButton_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				//Setting up a new pop-up frame for venue button
-				//VENUEPOP vp = new VENUEPOP();
-				//vp.NewScreen();
-				
+				LOGINPOP loginn3 = new LOGINPOP();
+				loginn3.lg();
 			}
 		});
-		btnNewButton_2.setToolTipText("A list of all the venues");
-		btnNewButton_2.setBounds(66, 10, 273, 132);
-		panel_3.add(btnNewButton_2_2);
-	
+		btnNewButton_4.setBounds(936, 189, 282, 141);
+		btnNewButton_4.setToolTipText("A list of all the venues");
+		panel_3.add(btnNewButton_4);
+		
+		
+		
 	}
 }
