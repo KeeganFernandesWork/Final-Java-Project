f06a75b45b31ac1810e7cf880a57f90ba3d65000
GhoshAnujit <118505475+GhoshAnujit@users.noreply.github.com>
2023-05-05 15:10:34 +0530

    changes

--- src/pack/LOGINPOP.java
+++ src/pack/LOGINPOP.java
@@ -9,6 +9,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JPanel;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.JLayeredPane;
+import javax.swing.JLabel;
 
 public class LOGINPOP {
 
@@ -42,18 +43,19 @@ public class LOGINPOP {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 533, 465);
+		frame.setBounds(100, 100, 729, 601);
 		frame.getContentPane().setLayout(null);
 		
-		JLayeredPane layeredPane = new JLayeredPane();
-		layeredPane.setBounds(0, 0, 1, 1);
-		frame.getContentPane().add(layeredPane);
-		layeredPane.setLayout(null);
-		
 		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 520, 427);
-		layeredPane.setLayer(panel,0);
-		layeredPane.add(panel);
-		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		panel.setBounds(0, 0, 715, 564);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel topRightLabel = new JLabel("New label");
+		topRightLabel.setBounds(335, 0, 380, 291);
+		panel.add(topRightLabel);
+		
+		
+		
+			}
 }
--- src/pack/Newframe.java
+++ src/pack/Newframe.java
@@ -216,7 +216,6 @@ public class Newframe extends JFrame {
 				//Setting up a new pop-up frame for venue button
 				VENUEPOP vp = new VENUEPOP();
 				vp.NewScreen();
-				
 			}
 		});
 		btnNewButton_2.setToolTipText("A list of all the venues");
